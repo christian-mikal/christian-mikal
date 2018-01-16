b7a2c250ee6f41f9795625cac35b85717cd6eff7
christian-mikal <christianmikalcarlsen@gmail.com>
2018-01-16 09:00:49 +0100

    minor changes

--- risk/rediger.js
+++ risk/rediger.js
@@ -1,3 +1,6 @@
+//@ts-check
+
+
 function setup() {
     var config = {
         apiKey: "AIzaSyDEB6KU4JvH0QC6gENK5zSnqCdsL6Mq5AI",
@@ -11,5 +14,54 @@ function setup() {
 
     let database = firebase.database();
 
-   
+    let inpLand = document.getElementById("land");
+    let inpRegion = document.getElementById("region");
+
+    let btnLagreLand = document.getElementById("lagreland");
+    btnLagreLand.addEventListener("click", lagreLand);
+
+    let inpKort = document.getElementById("kort");
+    let inpAntallNye = document.getElementById("antallnye");
+
+    let btnLagreKort = document.getElementById("lagrekort");
+    btnLagreKort.addEventListener("click", lagreKort);
+
+
+
+
+    let inpSpiller = document.getElementById("spiller");
+    let inpNavn = document.getElementById("navn");
+    let inpFarge = document.getElementById("farge");
+    let inpAlder = document.getElementById("alder");
+
+
+    let btnLagreSpiller = document.getElementById("lagrespiller");
+    btnLagreSpiller.addEventListener("click", lagreSpiller);
+
+
+    function lagreSpiller(e) {
+        let spiller = inpSpiller.value;
+        let  farge= inpFarge.value;
+        let  alder= inpAlder.value;
+        let  navn= inpNavn.value;
+        let ref = database.ref("spiller/" + spiller);
+        ref.set({ alder, farge, navn });
+    }
+
+
+
+
+    function lagreLand(e) {
+        let land = inpLand.value;
+        let kortid = inpRegion.value;
+        let ref = database.ref("land/" + land);
+        ref.set({ kortid });
+    }
+
+    function lagreKort(e) {
+        let kort = inpKort.value;
+        let antallnye = inpAntallNye.value;
+        let ref = database.ref("kort/" + kort);
+        ref.set({ antallnye });
+    }
 }
